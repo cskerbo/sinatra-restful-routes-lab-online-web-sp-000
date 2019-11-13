021ac62b373abd557fa0d6460cfe281a9de7b9fc
Chris Kerbo <chris.kerbo@outlook.com>
2019-11-13 04:16:59 +0000

    Automatically backed up by Learn

--- db/migrate/20191113041516_create_recipes.rb
+++ db/migrate/20191113041516_create_recipes.rb
@@ -1,6 +1,6 @@
 class CreateRecipes < ActiveRecord::Migration
   def change
-    creat_table :recipes do |x|
+    create_table :recipes do |x|
       x.string :name
       x.string :ingredients
       x.string :cook_time
